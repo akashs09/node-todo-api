4fe0c6a6a20b18e367b3d0266684740ca8cea595
Akash Sharma <sharma.akash028@gmail.com>
2018-08-14 18:38:13 -0400

    fdf

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.delete('/todos/:id', (req,res) => {
   }
   Todo.findByIdAndRemove(id).then((todo) => {
     if(!todo) {
-      return.status(404).send();
+      return res.status(404).send();
     }
     res.send(todo);
   }).catch((e) => {
